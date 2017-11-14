65797eaf4a0cfea932c67bfcb65122671e2597cb
zhuyanwei <zhuyanweixyz@gmail.com>
2017-11-14 13:31:26 +0800

    test ViBe

--- EmptyConsole/main.cpp
+++ EmptyConsole/main.cpp
@@ -1,15 +1,59 @@
+// This is based on 
+// "VIBE: A POWERFUL RANDOM TECHNIQUE TO ESTIMATE THE BACKGROUND IN VIDEO SEQUENCES"
+// by Olivier Barnich and Marc Van Droogenbroeck
+// Author : zouxy
+// Date   : 2013-4-13
+// HomePage : http://blog.csdn.net/zouxy09
+// Email  : zouxy09@qq.com
+
+#include "opencv2/opencv.hpp"
+#include "ViBe_BGS.h"
 #include <iostream>
-#include <opencv2/opencv.hpp>
+#include <cstdio>
+
 using namespace cv;
+using namespace std;
+
 int main()
 {
-	std::cout << "hello";
+	Mat frame, gray, mask;
+	VideoCapture capture;
+	capture.open("MySrc/Curtain.avi");
 
-	Mat m1;
-	m1 = imread("lena.jpg");
-	imshow("ss", m1);
-	waitKey();
-	int i;
-	std::cin >> i;
+	if (!capture.isOpened())
+	{
+		cout << "No camera or video input!\n" << endl;
+		return -1;
+	}
+
+	ViBe_BGS Vibe_Bgs;
+	int count = 0;
+
+	while (1)
+	{
+		count++;
+		capture >> frame;
+		if (frame.empty())
+			continue;
+		cvtColor(frame, gray, CV_RGB2GRAY);
+
+		if (count == 1)
+		{
+			Vibe_Bgs.init(gray);
+			Vibe_Bgs.processFirstFrame(gray);
+			cout << " Training GMM complete!" << endl;
+		}
+		else
+		{
+			Vibe_Bgs.testAndUpdate(gray);
+			mask = Vibe_Bgs.getMask();
+			morphologyEx(mask, mask, MORPH_OPEN, Mat());
+			imshow("mask", mask);
+		}
+
+		imshow("input", frame);
+		waitKey(30);
+	}
+
 	return 0;
 }
