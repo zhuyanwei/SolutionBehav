5e14a220e4752a61ddb41806cfd641f4deb3f796
zhuyanwei <zhuyanweixyz@gmail.com>
2018-01-18 18:46:43 +0800

    finish window

--- BehavMFC/BehavMFCDlg.cpp
+++ BehavMFC/BehavMFCDlg.cpp
@@ -93,7 +93,6 @@ void CBehavMFCDlg::OnPaint()
 {
 	if (IsIconic())
 	{
-		printf("onPaint isiconic");
 		CPaintDC dc(this); // 用于绘制的设备上下文
 		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
 		// 使图标在工作区矩形中居中
@@ -108,11 +107,17 @@ void CBehavMFCDlg::OnPaint()
 	}
 	else
 	{
-		printf("onpaint else");
-		CDialogEx::OnPaint();
-		//CDialog::OnPaint();                    // 重绘对话框
-		//CDialog::UpdateWindow();                // 更新windows窗口
-		//ShowImage(TheImage, IDC_ShowImg);
+		CDialogEx::OnPaint();  
+		////从资源中加载图片  
+		//CBitmap bitmap;
+		////加载指定位图资源 Bmp图片ID  
+		//bitmap.LoadBitmap(IDB_BitBackgroud);
+		////获取对话框上的句柄 图片控件ID  
+		//CStatic *p = (CStatic *)GetDlgItem(IDC_Back);
+		////设置静态控件窗口风格为位图居中显示  
+		//p->ModifyStyle(0xf, SS_BITMAP | SS_CENTERIMAGE);
+		////将图片设置到Picture控件上  
+		//p->SetBitmap(bitmap);
 	}
 }
 void CBehavMFCDlg::OnSysCommand(UINT nID, LPARAM lParam)
@@ -369,13 +374,13 @@ void CBehavMFCDlg::OnBnClickedCatch()
 
 void CBehavMFCDlg::OnBnClickedProcess()
 {
-	char VideosName[100];
-	ImgNum = ImgNum + 1;
-	sprintf_s(VideosName, "%s%.2d%s", "MyOutput/CatchedPictures/", ImgNum, ".avi");
-	CString str;
-	str.Format(_T("this-%s"), &VideosName);
-	MessageBox(str);
-
+	//获取对话框上的句柄 图片控件ID  
+	CStatic *p = (CStatic *)GetDlgItem(IDC_Back);
+	//设置静态控件窗口风格为位图居中显示  
+	p->MoveWindow(500,500,100,100);
+	//p->ModifyStyle(0xf, SS_BITMAP | SS_CENTERIMAGE);
+	////将图片设置到Picture控件上  
+	//p->SetBitmap(bitmap);
 	//int i = 10;
 	//CString str;
 	//str.Format(_T("this%s"),choose);
